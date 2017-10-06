38ee6cb14559bba0f22bbbbbdb4ee1ffb9ea588a
Mohini Sindhu <mohini.sindhu14@gmail.com>
2017-10-06 13:27:28 +0530

    ignore file

---
+++ ChatApplicationDemo/Classes/DataManager/temp/temp.h
@@ -0,0 +1,13 @@
+//
+//  temp.h
+//  ChatApplicationDemo
+//
+//  Created by Mohini Sindhu  on 06/10/17.
+//  Copyright © 2017 Mohini Sindhu . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface temp : NSObject
+
+@end
---
+++ ChatApplicationDemo/Classes/DataManager/temp/temp.m
@@ -0,0 +1,13 @@
+//
+//  temp.m
+//  ChatApplicationDemo
+//
+//  Created by Mohini Sindhu  on 06/10/17.
+//  Copyright © 2017 Mohini Sindhu . All rights reserved.
+//
+
+#import "temp.h"
+
+@implementation temp
+
+@end
